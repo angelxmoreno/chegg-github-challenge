adeb0c4d63927387dbe202a5e547a42b6b33a85b
Angel S. Moreno <angelxmoreno@gmail.com>
2018-01-17 18:21:11 -0500

    Added cache to Github api service

--- public/angular/services/github.js
+++ public/angular/services/github.js
@@ -4,9 +4,23 @@
     const GITHUB_URL = 'https://api.github.com';
 
     function config(CacheFactoryProvider) {
+        angular.extend(CacheFactoryProvider.defaults, {
+            maxAge: 5 * 1000
+        });
     }
 
     function service($http, $q, CacheFactory) {
+        const CACHE_NAME = 'githubCache';
+
+        if (!CacheFactory.get(CACHE_NAME)) {
+            CacheFactory.createCache(CACHE_NAME, {
+                deleteOnExpire: 'aggressive',
+                recycleFreq: 60000,
+                storageMode: 'memory'
+            });
+        }
+
+        const cache = CacheFactory.get(CACHE_NAME);
 
         this.token = undefined;
 
@@ -25,16 +39,21 @@
         this.repos = () => {
             return this.callApi('/user/repos');
         };
-        
+
         this.issues = (repo_id) => {
             return this.callApi(`/repos/${repo_id}/issues`);
         };
-        
+
         this.collaborators = (repo_id) => {
             return this.callApi(`/repos/${repo_id}/collaborators`)
         };
-        
+
         this.callApi = (uri) => {
+            if (cache.get(uri)) {
+                console.log('cache.get(uri)', cache.get(uri))
+                return $q.when(cache.get(uri));
+            }
+
             return $http({
                 method: 'GET',
                 url: GITHUB_URL + uri,
@@ -42,11 +61,12 @@
                     access_token: this.token
                 }
             }).then((response) => {
+                cache.put(uri, response.data)
                 return response.data;
             });
         }
     }
-    
+
     angular
             .module('github', [
                 'angular-cache'
