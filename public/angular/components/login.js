794c394877ed02592116cfe81c9b74a59a80e4d2
Angel S. Moreno <angelxmoreno@gmail.com>
2018-01-17 16:23:41 -0500

    Created simple login component

---
+++ public/angular/components/login.js
@@ -0,0 +1,36 @@
+"use strict";
+
+(function () {
+    const name = 'login';
+
+    function Controller($state, AuthService) {
+        this.formData = {};
+        console.log(AuthService.getApiKey());
+        
+        this.loginAction = () => {
+            let api_key = this.formData.api_key;
+            AuthService.setApiKey(api_key);
+        };
+        
+        this.goToList = () => {
+            $state.transitionTo('repoList');
+        };
+        
+        if(AuthService.getApiKey()){
+            console.info('Api Key found');
+            this.goToList();
+        } else {
+            console.info('Api Key not found');
+        }
+    }
+
+    const componentConfig = {
+        templateUrl: `/views/${name}.html`,
+        controller: ['$state','AuthService', Controller],
+        bindings: {}
+    };
+
+    angular
+            .module('GApp')
+            .component(name, componentConfig);
+})();
